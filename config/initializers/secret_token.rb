4fc66b0571685bd755c6f80706adb2b421829d65
underhilllabs <underhilllabs@gmail.com>
2012-06-06 16:40:21 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookies::Application.config.secret_token = '49c6cc128e9b3edee5b183b13f342e3117a47724b85abb36b63a5658ffdf5c8942e2729c84f05d5b3234e830875113f619d0b72ec7581eced0dbfef5fa931679'

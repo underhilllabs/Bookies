ed9860a168aecd4fc4db7774b9331aa5e81f071a
underhilllabs <underhilllabs@gmail.com>
2012-06-13 16:22:06 -0600

    added :dependent => :destroy to has_many relationships in models so they will be de;eted when the belongs_to object is deleted.

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ActiveRecord::Base
   attr_accessible :desc, :private, :title, :url, :user_id, :tags
   belongs_to :user
-  has_many :tags
+  has_many :tags, :dependent => :destroy
   validates_presence_of :url, :on => :create
   validates_presence_of :title, :on => :create
   validates :user_id, :presence => true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :desc, :email, :fullname, :password_digest, :pic_url, :username, :website, :website2, :website3, :password, :password_confirmation
   has_secure_password
-  has_many :bookmarks
+  has_many :bookmarks, :dependent => :destroy
   has_many :tags
   validates_presence_of :password, :on => :create
   validates_presence_of :email, :presence => true, :uniqueness => true, :email_format => true

50b54d1d041f4bd9fa5d369726c1963c8fb57cee
underhilllabs <underhilllabs@gmail.com>
2012-06-14 17:18:26 -0600

    added session username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    session[:username] = user.username
     redirect_to root_url, notice: "Signed in!"
-    # user = User.find_by_email(params[:email])
-    # if user && user.authenticate(params[:password])
-    #   session[:user_id] = user.id
-    #   session[:username] = user.username
-    #   redirect_to root_url, :notice => "Logged in!"
-    # else
-    #   flash.now.alert = "Invalid email or password"
-    #   render "new"
-    # end
   end
 
   def destroy
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user.provider == "twitter"
       user.pic_url
     else 
-      gravatar_id = Digest::MD5::hexdigest(@user.email).downcase
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
       "http://gravatar.com/avatar/#{gravatar_id}.png?d=#{APP_CONFIG["gravatar_type"]}&s=#{img_size}" 
     end
   end

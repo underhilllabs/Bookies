bdaefae771ef24920caaf81c17c7fbb53b43e1e5
underhilllabs <underhilllabs@gmail.com>
2012-06-19 01:19:16 -0600

    updated tag model
    added bookmark_id to attr_accessible

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   belongs_to :bookmark
   belongs_to :user
-  attr_accessible :name
+  attr_accessible :name, :bookmark_id
   validates_presence_of :name, :on => :create
   # so you can do: @bookmark.tags.join(", ")
   def to_s

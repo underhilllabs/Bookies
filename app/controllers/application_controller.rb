aea67e66c141cfb6080d3f22fdb2b6a38ca0d68c
Bart Lantz <bartlantz@gmail.com>
2013-01-12 11:20:22 -0700

    added render time output to each page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_start_time
+
+  def set_start_time
+    @start_time = Time.now.usec
+  end
   private
 
   def current_user

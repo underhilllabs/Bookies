83732bfdc82d014ad7873840c190892713e910fa
Bart Lantz <bartlantz@gmail.com>
2013-01-15 00:38:39 -0700

    added save to add_bookmark method

--- app/helpers/bookmark_helper.rb
+++ app/helpers/bookmark_helper.rb
@@ -1,9 +1,11 @@
 module BookmarkHelper
   def add_bookmark(href, desc, extended, tags, time, hashed_url )
     @Bookmark = Bookmark.new
+    @Bookmark.user_id = session[:user_id]
     @Bookmark.url = href
     @Bookmark.title = desc
     @Bookmark.created_at = Time.now
     @Bookmark.hashed_url = Digest::MD5.hexdigest(href)
+    @Bookmark.save
   end
 end

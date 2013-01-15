0001641ccda46d76f6e2154dd8e6dc6a2acd2529
Bart Lantz <bartlantz@gmail.com>
2013-01-15 00:29:08 -0700

    added new bookmark_helper function

--- app/helpers/bookmark_helper.rb
+++ app/helpers/bookmark_helper.rb
@@ -1,2 +1,9 @@
 module BookmarkHelper
+  def add_bookmark(href, desc, extended, tags, time, hashed_url )
+    @Bookmark = Bookmark.new
+    @Bookmark.url = href
+    @Bookmark.title = desc
+    @Bookmark.created_at = Time.now
+    @Bookmark.hashed_url = Digest::MD5.hexdigest(href)
+  end
 end

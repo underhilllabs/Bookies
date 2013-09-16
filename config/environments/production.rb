bee70664628cd3186e517589dfa291b785fd082f
Bart Lantz <underhilllabs@gmail.com>
2013-09-15 21:47:21 -0400

    updated config files for rails 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,12 +4,15 @@ Bookies::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  # required railsd 4
+  config.eager_load = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true

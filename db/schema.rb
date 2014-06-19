112776f7b86d8a2145b76039bc4a160eda9c9553
Bart Lantz <underhilllabs@gmail.com>
2014-06-19 14:52:50 -0600

    Adds delayed_job for importing bookmarks

--- db/schema.rb
+++ db/schema.rb
@@ -9,51 +9,67 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114035250) do
+ActiveRecord::Schema.define(version: 20140619205154) do
 
-  create_table "bookmarks", :force => true do |t|
+  create_table "bookmarks", force: true do |t|
     t.string   "url"
     t.string   "title"
     t.text     "desc"
     t.datetime "created"
     t.datetime "modified"
     t.boolean  "private"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-    t.integer  "user_id",    :default => 1
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    default: 1
     t.string   "hashed_url"
   end
 
-  create_table "followings", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "following_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "identities", :force => true do |t|
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "followings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "following_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "identities", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "username"
     t.string   "nickname"
   end
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string   "name"
     t.integer  "bookmark_id"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.integer  "user_id",     :default => 1
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "user_id",     default: 1
   end
 
-  add_index "tags", ["bookmark_id"], :name => "index_tags_on_bookmark_id"
+  add_index "tags", ["bookmark_id"], name: "index_tags_on_bookmark_id", using: :btree
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "username"
     t.string   "email"
     t.string   "fullname"
@@ -62,8 +78,8 @@ ActiveRecord::Schema.define(:version => 20130114035250) do
     t.string   "website2"
     t.string   "website3"
     t.string   "pic_url"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "provider"
     t.string   "uid"
   end

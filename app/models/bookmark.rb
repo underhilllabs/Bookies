ead2ee4ec0c37280565ed60ae6bc7d8565f98d79
Bart Lantz <underhilllabs@gmail.com>
2014-06-23 13:52:27 -0600

    Adds bookmark scopes: private and public

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,8 +2,11 @@ class Bookmark < ActiveRecord::Base
   attr_accessible :desc, :private, :title, :url, :user_id, :tags, :hashed_url
   belongs_to :user
   has_many :tags, :dependent => :destroy
+
   validates_presence_of :url, :on => :create
   validates_presence_of :title, :on => :create
   validates :user_id, :presence => true
-  #scope :public, where("private != ?", true)
+
+  scope :public, where(private: :nil)
+  scope :private, where(private: true)
 end

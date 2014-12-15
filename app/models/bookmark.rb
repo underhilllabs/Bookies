bf60358053fa98f3c5708135751e5f8498b33d4d
Bart Lantz <underhilllabs@gmail.com>
2014-12-15 09:41:25 -0700

    Adds archived scope and updates bookmark field to is_archived

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ActiveRecord::Base
-  attr_accessible :desc, :private, :title, :url, :user_id, :tags, :hashed_url, :archived, :archive_url
+  attr_accessible :desc, :private, :title, :url, :user_id, :tags, :hashed_url, :is_archived, :archive_url
   belongs_to :user
   has_many :tags, :dependent => :destroy
 
@@ -9,4 +9,5 @@ class Bookmark < ActiveRecord::Base
 
   scope :published, -> { where(private: :nil) }
   scope :unpublished, -> { where(private: true) }
+  scope :archived, -> { where(is_archived: true) }
 end

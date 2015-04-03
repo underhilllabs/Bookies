ba9a4e74ec15d5ca9c161c441d053fc300781931
Bart Lantz <underhilllabs@gmail.com>
2015-04-03 00:31:38 -0600

    fixes issue with readability

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -15,15 +15,17 @@ class Bookmark < ActiveRecord::Base
 
   # download and archive the bookmark
   def archive_the_url
+    logger.info "archiving the url"
     mechanize = Mechanize.new
     source = mechanize.get(url).body
-    b = Bookmark.find(id)
     File.open("public/archive/#{id}.html", "w") do |f|
+      logger.info "archiving ID: #{id}"
       f.write("<h1>#{title}</h1>\n<hr />\n")
-      f.write(Readability::Document.new( source, tags: @tags, attributes: @attributes ).content)
-      b.is_archived = true
-      b.archive_url = "/archive/#{id}.html"
-      b.save
+      f.write(Readability::Document.new( source, tags => %w[div p ul li ol pre img a h1 h2 h3 h4 h5] , attributes =>  %w[src href width height] ).content)
+      #b = Bookmark.find(id)
+      is_archived = true
+      archive_url = "/archive/#{id}.html"
+      save
     end
   end
 

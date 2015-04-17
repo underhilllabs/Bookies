b21519f8fd86e299e795fcc3bd8e2c7d30375913
Bart Lantz <underhilllabs@gmail.com>
2015-04-17 10:30:23 -0600

    Updates README for Resque

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,4 @@
 class Bookmark < ActiveRecord::Base
-  @@archive_tags = %w[div p ul li ol pre img a h1 h2 h3 h4 h5]
-  @@archive_attributes = %w[src href width height]
   attr_accessible :desc, :private, :title, :url, :user_id, :tags, :hashed_url, :is_archived, :archive_url
   belongs_to :user
   has_many :tags, :dependent => :destroy
@@ -17,18 +15,5 @@ class Bookmark < ActiveRecord::Base
   def archive_the_url
     logger.info "Queued the archiving of the url"
     Resque.enqueue(BookmarkArchiver, id)
-    # mechanize = Mechanize.new
-    # source = mechanize.get(url).body
-    # File.open("public/archive/#{id}.html", "w") do |f|
-    #   logger.info "archiving ID: #{id}"
-    #   f.write("<h1>#{title}</h1>\n<hr />\n")
-    #   f.write(Readability::Document.new( source, tags => %w[div p ul li ol pre img a h1 h2 h3 h4 h5] , attributes =>  %w[src href width height] ).content)
-    #   #b = Bookmark.find(id)
-    #   is_archived = true
-    #   archive_url = "/archive/#{id}.html"
-    #   save
-    # end
   end
-
-  # handle_asynchronously :archive_the_url
 end

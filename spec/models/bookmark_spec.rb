371616f141a2acb771a85a5a5a2db626d72baa7c
Bart Lantz <underhilllabs@gmail.com>
2015-04-29 17:04:56 -0600

    Adds spec for bookmark model

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,2 +1,28 @@
+require 'rails_helper'
+
 describe Bookmark do
+  let(:bookmark) { Bookmark.new }
+  # let(:user) { User.new }
+  describe "validation" do
+
+    it "fails without a URL" do
+      bookmark.title = "test"
+      bookmark.url = nil
+      expect(bookmark).to_not be_valid
+    end
+
+    it "fails without a title" do
+      bookmark.title = nil
+      bookmark.url = "http://github.com"
+      expect(bookmark).to_not be_valid
+    end
+
+    it "fails without a user" do
+      bookmark.title = "test"
+      bookmark.url =  "http://github.com"
+      bookmark.user = nil
+      expect(bookmark).to_not be_valid
+    end
+
+  end
 end

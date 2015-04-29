9178580590fd58090d8fb7d9edbb6c34d4edbc3d
Bart Lantz <underhilllabs@gmail.com>
2015-04-28 23:04:55 -0600

    Adds test for bookmark model

--- test/unit/bookmark_test.rb
+++ test/unit/bookmark_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class BookmarkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validates bookmarked owned by user" do
+    @bookmark1 = bookmarks(:one)
+    @bookmark1.user = ''
+    assert_not_true @bookmark1.valid?
+  end
+
+  test "validates url" do
+    @bookmark1 = bookmarks(:one)
+    @bookmark1.url = ''
+    assert_not_true @bookmark1.valid?
+  end
+
+  test "validates title" do
+    @bookmark1 = bookmarks(:one)
+    @bookmark1.title = ''
+    assert_not_true @bookmark1.valid?
+  end
 end

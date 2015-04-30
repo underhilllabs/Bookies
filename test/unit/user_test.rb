c380da145fc30c3bbca44a3d35055e20dcef3e44
Bart Lantz <underhilllabs@gmail.com>
2015-04-30 07:47:23 -0600

    adds user model tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validates username" do
+    @user1 = users(:bossy)
+    @user1.username = ''
+    assert_not @user1.valid?
+  end
+
+  test "validates email presence" do
+    @user1 = users(:bossy)
+    @user1.email = ''
+    assert_not @user1.valid?
+  end
+
+  test "validates email uniqueness" do
+    @user1 = users(:bossy)
+    @user2 = users(:seamus)
+    @user1.email = @user2.email
+    assert_not @user1.valid?
+  end
 end

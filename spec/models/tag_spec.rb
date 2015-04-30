1edb133abbdb57ef14a98633630d299ca3a17750
Bart Lantz <underhilllabs@gmail.com>
2015-04-30 11:36:43 -0600

    adds specs for tag model

---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe Tag do
+  let(:tag) { Tag.new }
+  describe "validation" do
+
+    it "fails without a name" do
+      tag.name = nil
+      expect(tag).to_not be_valid
+    end
+
+    it "fails without a bookmark" do
+      tag.name = "test"
+      tag.user_id = 1
+      tag.bookmark_id = nil
+      expect(tag).to_not be_valid
+    end
+
+    it "fails without a user" do
+      tag.name = "test"
+      tag.bookmark_id = 1
+      tag.user_id = nil
+      expect(tag).to_not be_valid
+    end
+
+  end
+end

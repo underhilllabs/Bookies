32e1212cea888dd08143d3972ce5da51c8fdd147
Bart Lantz <underhilllabs@gmail.com>
2015-04-30 07:45:29 -0600

    changes log_level to warn, from debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Bookies::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]

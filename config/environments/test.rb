6d0df38f57843bd8855d0ac3aed96ae003afbf71
Bart Lantz <underhilllabs@gmail.com>
2015-05-02 09:00:17 -0600

    updates test environment to remove deprecation error

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,11 @@
 Bookies::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # set test order to avoid deprecation warning
+  Rails.application.configure do
+    config.active_support.test_order = :sorted
+  end
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped

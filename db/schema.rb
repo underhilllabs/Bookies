d5f86b7b18dd382473adaf6c4787fc964e1e8041
Bart Lantz <underhilllabs@gmail.com>
2015-05-10 22:18:23 -0600

    adds db migrations for acts-as-taggable-on

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141215163057) do
+ActiveRecord::Schema.define(version: 20150510205737) do
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string   "url",         limit: 255
+    t.string   "url",         limit: 2048
     t.string   "title",       limit: 255
     t.text     "desc",        limit: 65535
     t.datetime "created"
     t.datetime "modified"
     t.boolean  "private",     limit: 1
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id",     limit: 4,     default: 1
     t.string   "hashed_url",  limit: 255
     t.string   "archive_url", limit: 255
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 20141215163057) do
   end
 
   create_table "delayed_jobs", force: :cascade do |t|
-    t.integer  "priority",   limit: 4,     default: 0, null: false
-    t.integer  "attempts",   limit: 4,     default: 0, null: false
-    t.text     "handler",    limit: 65535,             null: false
+    t.integer  "priority",   limit: 4,        default: 0, null: false
+    t.integer  "attempts",   limit: 4,        default: 0, null: false
+    t.text     "handler",    limit: 16777215
     t.text     "last_error", limit: 65535
     t.datetime "run_at"
     t.datetime "locked_at"
@@ -61,29 +61,49 @@ ActiveRecord::Schema.define(version: 20141215163057) do
     t.string   "nickname",        limit: 255
   end
 
-  create_table "tags", force: :cascade do |t|
+  create_table "old_tags", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.integer  "bookmark_id", limit: 4
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at"
     t.integer  "user_id",     limit: 4,   default: 1
   end
 
-  add_index "tags", ["bookmark_id"], name: "index_tags_on_bookmark_id", using: :btree
+  add_index "old_tags", ["bookmark_id"], name: "index_tags_on_bookmark_id", using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 255
-    t.string   "email",      limit: 255
-    t.string   "fullname",   limit: 255
-    t.string   "website",    limit: 255
-    t.text     "desc",       limit: 65535
-    t.string   "website2",   limit: 255
-    t.string   "website3",   limit: 255
-    t.string   "pic_url",    limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.string   "provider",   limit: 255
-    t.string   "uid",        limit: 255
+    t.string   "username",        limit: 255
+    t.string   "email",           limit: 255
+    t.string   "fullname",        limit: 255
+    t.string   "website",         limit: 255
+    t.text     "desc",            limit: 65535
+    t.string   "website2",        limit: 255
+    t.string   "website3",        limit: 255
+    t.string   "pic_url",         limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "provider",        limit: 255
+    t.string   "uid",             limit: 255
+    t.string   "password_digest", limit: 255
   end
 
 end

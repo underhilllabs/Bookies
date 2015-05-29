69567ab681ba286a49b4ee5bd6b03d2f40e0426f
Bart Lantz <underhilllabs@gmail.com>
2015-05-28 18:15:06 -0600

    updates workers

--- app/workers/bookmark_archiver.rb
+++ app/workers/bookmark_archiver.rb
@@ -6,14 +6,43 @@ class BookmarkArchiver
   def self.perform(bookmark_id)
     b = Bookmark.find(bookmark_id)
     mechanize = Mechanize.new
+    page = mechanize.get(b.url)
+    source = page.body
+    fname = page.filename
+    if /(pdf|png|jpg|gif|gifv)$/.match(fname) then
+      ext = /(pdf|png|jpg|gif|gifv)$/.match(fname)[1]
+      File.open("public/archive/#{b.id}.#{ext}", "wb") do |f|
+        f.write(source)
+      end
+    else
+      ext = "html"
+      File.open("public/archive/#{b.id}.html", "w") do |f|
+        f.write("<h1>#{b.title}</h1>\n<hr />\n")
+        f.write(Readability::Document.new( source, :tags => @@archive_tags, :attributes => @@archive_attributes ).content)
+      end
+    end
+    Archive.find_or_create_by(bookmark_id: b.id, location: b.archive_url, user_id: b.user_id, url: b.url, filetype: ext)
+    b.is_archived = true
+    b.archive_url = "/archive/#{b.id}.#{ext}"
+    b.save
+  end
+  
+  def save_web_file(b)
     source = mechanize.get(b.url).body
     File.open("public/archive/#{b.id}.html", "w") do |f|
-      #logger.info "archiving ID: #{b.id}"
       f.write("<h1>#{b.title}</h1>\n<hr />\n")
       f.write(Readability::Document.new( source, :tags => @@archive_tags, :attributes => @@archive_attributes ).content)
       b.is_archived = true
       b.archive_url = "/archive/#{b.id}.html"
+      Archive.find_or_create_by(bookmark_id: b.id, location: b.archive_url, user_id: b.user_id, url: b.url)
       b.save
+    end
+  end
+
+  def save_binary_file(b)
+    source = mechanize.get(b.url).body
+    File.open("public/archive/#{b.id}.html", "w") do |f|
+      Archive.find_or_create_by(bookmark_id: b.id, location: b.archive_url, user_id: b.user_id, url: b.url)
     end
   end
 end
--- app/workers/bookmark_importer.rb
+++ app/workers/bookmark_importer.rb
@@ -1,5 +1,21 @@
-class BookmarkWorker
+class BookmarkImporter
   @queue = :bookmark_import_queue
-  def self.perform(user_id)
+  def self.perform(user_id, file_name)
+    doc = Nokogiri.XML(file_name)
+    doc.xpath('/posts/post').map do |p|
+      time = Time.parse(p["time"])
+      b = Bookmark.create_with(desc: p["extended"], url: p["href"], created_at: time, updated_at: time, title: p["description"]).find_or_create_by(:user_id => user_id, :hashed_url => p["hash"])
+      if p["tag"] then
+        tags = p["tag"]
+        tags = tags.split(" ").map do |tag|
+          Tag.new(:name => tag.strip)
+        end
+        if b.save then
+          tags.each { |t| t.bookmark_id = b.id; t.save }
+        end
+      else
+        b.save
+      end
+    end
   end
 end

4c6baad7241c038ce91082576a37849080fc95f2
Bart Lantz <underhilllabs@gmail.com>
2015-12-18 11:56:41 -0500

    updates assets version

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,6 @@ module Bookies
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end

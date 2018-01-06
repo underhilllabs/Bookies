7a9143b67680e511ce95e9e4c4643cc15f38adc8
Bart Lantz <underhilllabs@gmail.com>
2018-01-06 12:37:08 -0700

    changes deprecated before_filter to before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_start_time
+  before_action :set_start_time
 
   def set_start_time
     @start_time = Time.now.usec

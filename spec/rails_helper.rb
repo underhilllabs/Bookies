8781a980ea1b370841e6b128c738469f194d1be5
Bart Lantz <underhilllabs@gmail.com>
2018-01-07 13:00:15 -0700

    Update rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].sort.each {|f| require f}
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.reverse.each {|f| require f}
 #require 'spec/support/features'
 #require 'spec/support/features/session_helpers.rb'
 

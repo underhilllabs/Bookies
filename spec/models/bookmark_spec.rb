9523f473dd83a34dc83a4f2f8eb50dfc19f76840
Bart Lantz <underhilllabs@gmail.com>
2018-01-21 19:50:15 -0700

    updates bookmark model spec

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 describe Bookmark do
   let(:bookmark) { Bookmark.new }
-  # let(:user) { User.new }
-  describe "validation" do
+  let(:user) { User.create(username: "Tester") }
+  describe "validations" do
 
     it "fails without a URL" do
       bookmark.title = "test"
       bookmark.url = nil
+      bookmark.user = user
       expect(bookmark).to_not be_valid
     end
 
     it "fails without a title" do
       bookmark.title = nil
       bookmark.url = "http://github.com"
+      bookmark.user = user
       expect(bookmark).to_not be_valid
     end
 
@@ -24,11 +26,12 @@ describe Bookmark do
       expect(bookmark).to_not be_valid
     end
 
-    it "saves hashed_url when saved" do
+    it "saves bookmark with correct attrbutes" do
       bookmark.title = "test"
       bookmark.url =  "http://github.com"
-      bookmark.user = nil
-      expect(bookmark).to_not be_valid
+      bookmark.user = user
+      bookmark.save
+      expect(bookmark).to be_valid
     end
   end
 end
